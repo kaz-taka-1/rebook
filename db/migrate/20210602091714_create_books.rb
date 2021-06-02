adf889351c0ccf298237296b87dc7f861636b5e6
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-06-02 18:30:09 +0900

    booksテーブルの作成

---
+++ db/migrate/20210602091714_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :book_title,       null: false 
+      t.string :author,           null: false 
+      t.integer :total_page,      null: false 
+      t.integer :read_page
+      t.references :user,         null: false , foreign_key: true
+      # t.references :preparation,  null: false , foreign_key: true
+      # t.references :output,       null: false , foreign_key: true
+      t.timestamps
+    end
+  end
+end
