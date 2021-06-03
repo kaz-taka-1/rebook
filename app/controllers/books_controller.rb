41842cb2d7dd0d410c8a3a509e0ac8a62e3b6af7
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-06-03 17:16:02 +0900

    登録したbook情報の表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,22 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.includes(:user).order("created_at DESC")
   end
 
   def new
     @book=Book.new
+  end
+  def create
+
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  def book_params
+    params.require(:book).permit(:book_title, :author, :total_page, :image).merge(user_id: current_user.id)
   end
 end
