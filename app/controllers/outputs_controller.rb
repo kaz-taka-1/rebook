e3a5b0b0632c86d28e3e0c0c3a961f244b60cc7d
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-06-09 20:09:02 +0900

    indexアクションのビューファイル修正

--- app/controllers/outputs_controller.rb
+++ app/controllers/outputs_controller.rb
@@ -12,6 +12,21 @@ class OutputsController < ApplicationController
       render :new
     end
   end
+  def edit
+    @book=Book.find(params[:book_id])
+    @output=Output.find_by(book_id:@book.id)
+  end
+  def update
+    @book=Book.find(params[:book_id])
+    @output=Output.new(output_params)
+    if @output.save
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+  
+
 
   def output_params
     params.require(:output).permit(:text).merge(book_id:params[:book_id])
--- app/controllers/preparations_controller.rb
+++ app/controllers/preparations_controller.rb
@@ -6,40 +6,32 @@ class PreparationsController < ApplicationController
 
   def new
     @book=Book.find(params[:book_id])
-    if Preparation.exists?(book_id:@book.id)
-      redirect_to book_path(@book.id)
-    else
-      @preparation=Preparation.new
-    end
-
+    @preparation=Preparation.new
   end
 
   def create
     @preparation=Preparation.new (preparation_params)
-    # binding.pry
     if @preparation.save
-      redirect_to book_preparations_path(@preparation.book_id)
+      redirect_to book_path(@preparation.book_id)
     else
       render :new
     end
   end
 
-  # def show
-  # end
   def edit
      @book = Book.find(params[:book_id])
      @preparation=Preparation.find_by(book_id:@book.id)
-
    end
 
-  # def update
-  #   @preparation=Preparation.news (preparation_params)
-  #   if @preparation.save
-  #     redirect_to book_preparations_path(@book.id)
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    @book= Book.find(params[:book_id])
+    @preparation=Preparation.new(preparation_params)
+    if @preparation.save
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
   def preparation_params
     # binding.pry
     params.require(:preparation).permit(:reason, :get_ability, :status, :prior_knowledge,:unknown).merge(book_id:params[:book_id])
