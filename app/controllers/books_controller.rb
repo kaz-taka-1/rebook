7488be7f7e0921927efa0e121cd381546248600a
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-06-10 09:51:48 +0900

    book 編集ページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,9 @@ class BooksController < ApplicationController
       render :new
     end
   end
+  def edit
+    @book= Book.find(params[:id])
+  end
 
   def show
     @book= Book.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'outputs/new'
   devise_for :users
   root to:"books#index"
-  resources :books, only:[:index, :new, :create, :show,:detroy] do
+  resources :books, only:[:index, :new, :create, :show,:detroy,:edit] do
     resources :preparations
     resources :outputs, only:[ :new, :create, :edit, :update, :detroy]
   end
