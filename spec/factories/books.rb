34c576fc5bbd6a64cc006583ac0e0f70ed6dd7d1
kaz-taka-1 <k.t.niconico.physics@gmail.com>
2021-09-17 16:11:38 +0900

    bookの単体テストコード

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     book_title              {Faker::Book.title}
     author                  {Faker::Book.author}
     total_page              {Faker::Number.number(digits: 3)}
+    image {Faker::Lorem.sentence}
     association :user 
   end
 end
--- spec/models/books_spec.rb
+++ spec/models/books_spec.rb
@@ -9,9 +9,27 @@ RSpec.describe Book, type: :model do
       it 'book_titleとautor、totalpageが存在すれば登録できる' do
         expect(@book).to be_valid
       end
+      it 'imageがなくても登録できる' do
+        @book.image = ''
+        expect(@book).to be_valid
+      end
     end
     context '新規本登録ができないとき' do
-      
+      it 'book_titleが空では登録できない' do
+        @book.book_title = ''
+        @book.valid?
+        expect(@book.errors.full_messages).to include("Book title can't be blank")
+      end
+      it 'authorが空では登録できない' do
+        @book.author = ''
+        @book.valid?
+        expect(@book.errors.full_messages).to include("Author can't be blank")
+      end
+      it 'が空では登録できない' do
+        @book.total_page = ''
+        @book.valid?
+        expect(@book.errors.full_messages).to include("Total page can't be blank")
+      end
     end
   end
 end
